f40518370bd689819733b5589573647033f17707
Raj Mourya <rajhmourya@gmail.com>
2022-04-03 16:21:37 +0530

    projects mvp is working

--- app/src/main/java/com/example/signalapp/MainActivity.java
+++ app/src/main/java/com/example/signalapp/MainActivity.java
@@ -7,37 +7,27 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.telephony.CellInfoGsm;
 import android.telephony.CellSignalStrength;
-import android.telephony.CellSignalStrengthGsm;
 import android.telephony.CellSignalStrengthLte;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import java.util.List;
 
-import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-import static android.Manifest.permission.READ_PHONE_NUMBERS;
-import static android.Manifest.permission.READ_PHONE_STATE;
-import static android.Manifest.permission.READ_SMS;
 
 public class MainActivity extends AppCompatActivity {
     TelephonyManager mTelephonyManager;
@@ -46,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
     Button wifibtn;
     Button databtn;
     NetworkInfo Info;
+
+    CheckBox strongcb;
+    CheckBox goodcb;
+    CheckBox faircb;
+    CheckBox weakcb;
 
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
@@ -56,27 +51,22 @@ public class MainActivity extends AppCompatActivity {
 
         wifibtn =  findViewById(R.id.wifibtn);
         databtn =  findViewById(R.id.databtn);
+        strongcb =  findViewById(R.id.strongcb);
+        goodcb =  findViewById(R.id.goodcb);
+        faircb =  findViewById(R.id.faircb);
+        weakcb =  findViewById(R.id.weakcb);
+
         SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.refreshLayout);
         swipeRefreshLayout.setOnRefreshListener(
                 new SwipeRefreshLayout.OnRefreshListener() {
                     @Override
                     public void onRefresh() {
-
                         checkNetwork();
                         swipeRefreshLayout.setRefreshing(false);
                     }
                 }
         );
 
-//        Button refresh = (Button) findViewById(R.id.btn_refresh);
-//
-//        //Listening to button event
-//        refresh.setOnClickListener(new View.OnClickListener() {
-//
-//            public void onClick(View arg0) {
-//                checkNetwork();
-//            }
-//        });
 
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         Info = cm.getActiveNetworkInfo();
@@ -106,6 +96,8 @@ public class MainActivity extends AppCompatActivity {
 
                 int rssi = wifiManager.getConnectionInfo().getRssi();
                 Log.d("wifisignal", "" + rssi + " dbm");
+                setCheckBox(rssi);
+                Toast.makeText(this, "wifi signal strength in dbm "+rssi, Toast.LENGTH_SHORT).show();
                 int level = WifiManager.calculateSignalLevel(rssi, 5);
                 System.out.println("Level is " + level + " out of 5");
 
@@ -119,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
                 databtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.round_button_red));
                 mPhoneStatelistener = new MyPhoneStateListener();
 
-                mTelephonyManager.listen(mPhoneStatelistener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+//                mTelephonyManager.listen(mPhoneStatelistener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
 
                 if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
                     ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
@@ -135,6 +127,8 @@ public class MainActivity extends AppCompatActivity {
                                 int snr = ((CellSignalStrengthLte) cellInfo).getRssnr();
                                 int rssi =  ((CellSignalStrengthLte) cellInfo).getRssi();
                                 System.out.println("rsrq "+rsrq+" rsrp "+rsrp+" snr "+snr+" rssi "+rssi);
+                                Toast.makeText(this, "mobile data signal strength in dbm "+rssi, Toast.LENGTH_SHORT).show();
+                                setCheckBox(rssi);
                             }
                         }
                     }
@@ -157,22 +151,7 @@ public class MainActivity extends AppCompatActivity {
             signalSupport = signalStrength.getGsmSignalStrength();
             Log.d(getClass().getCanonicalName(), "------ gsm signal --> " + signalSupport);
 
-            if (signalSupport > 30) {
-                Log.d(getClass().getCanonicalName(), "Signal GSM : Good");
-
-
-            } else if (signalSupport > 20 && signalSupport < 30) {
-                Log.d(getClass().getCanonicalName(), "Signal GSM : Avarage");
-
-
-            } else if (signalSupport < 20 && signalSupport > 3) {
-                Log.d(getClass().getCanonicalName(), "Signal GSM : Weak");
-
-
-            } else if (signalSupport < 3) {
-                Log.d(getClass().getCanonicalName(), "Signal GSM : Very weak");
-
-            }
+//            setCheckBox(signalSupport);
         }
     }
 
@@ -188,6 +167,51 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Log.d("mobiledata3",""+mTelephonyManager.getAllCellInfo());
                 }
+        }
+    }
+
+
+    public void setCheckBox(int rssi){
+
+        /*
+        * Excellent >-50 dBm
+
+        Good -50 to -60 dBm
+
+        Fair -60 to -70 dBm
+
+        Weak < -70 dBm
+        * */
+
+        if (rssi > -50) {
+            Log.d(getClass().getCanonicalName(), "Signal GSM : Strong");
+            strongcb.setChecked(true);
+            goodcb.setChecked(false);
+            faircb.setChecked(false);
+            weakcb.setChecked(false);
+            strongcb.setHintTextColor(Color.RED);
+        } else if (rssi >= -60 && rssi < -50) {
+            Log.d(getClass().getCanonicalName(), "Signal GSM : Good");
+            strongcb.setChecked(false);
+            goodcb.setChecked(true);
+            faircb.setChecked(false);
+            weakcb.setChecked(false);
+            goodcb.setHintTextColor(Color.RED);
+
+        } else if (rssi < -60 && rssi >= -70) {
+            Log.d(getClass().getCanonicalName(), "Signal GSM : Weak");
+            strongcb.setChecked(false);
+            goodcb.setChecked(false);
+            faircb.setChecked(true);
+            weakcb.setChecked(false);
+            faircb.setHintTextColor(Color.RED);
+
+        } else if (rssi <= -70) {
+            Log.d(getClass().getCanonicalName(), "Signal GSM : Very weak");
+            strongcb.setChecked(false);
+            goodcb.setChecked(false);
+            faircb.setChecked(false);
+            weakcb.setChecked(true);
         }
     }
 }
